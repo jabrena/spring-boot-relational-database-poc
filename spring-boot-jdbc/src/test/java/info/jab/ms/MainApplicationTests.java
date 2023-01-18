29948e43bfbbd707acb0abca92ecdb314999f227
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-01-18 17:32:12 +0100

    Adding a new example & the first requirement

---
+++ spring-boot-jdbc/src/test/java/info/jab/ms/MainApplicationTests.java
@@ -0,0 +1,20 @@
+package info.jab.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:tc:postgresql:15.1-alpine://tag",
+		"spring.datasource.username=postgres",
+		"spring.datasource.password=postgres"
+})
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
