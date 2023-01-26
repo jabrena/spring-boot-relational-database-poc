470c2ed30864c2a7b0af97b8bde6a5c26615449a
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-01-26 12:15:57 +0100

    fixing issue in jpa module

--- spring-data-jpa/src/main/java/info/jab/ms/controller/ActorController.java
+++ spring-data-jpa/src/main/java/info/jab/ms/controller/ActorController.java
@@ -5,11 +5,11 @@ import com.jab.ms.openapi.gen.model.ActorDto;
 import info.jab.ms.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class ActorController implements ApiApi{
