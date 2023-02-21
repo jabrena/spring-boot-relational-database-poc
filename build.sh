7f0cd156fa550d8b6961fa3a8b8c2a730e71a09b
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-02-21 18:36:05 +0100

    Adding in the script the rest of the builds (#74)

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-echo "Building images"
+echo "Building images for spring-boot-jdbc"
 mvn spring-boot:build-image -pl spring-boot-jdbc
 mvn -Pnative spring-boot:build-image -pl spring-boot-jdbc
+
+echo "Building images for spring-boot-jooq"
+mvn spring-boot:build-image -pl spring-boot-jooq
+mvn -Pnative spring-boot:build-image -pl spring-boot-jooq
+
+echo "Building images for spring-boot-mybatis"
+mvn spring-boot:build-image -pl spring-boot-mybatis
+mvn -Pnative spring-boot:build-image -pl spring-boot-mybatis
+
+echo "Building images for spring-data-jdbc"
+mvn spring-boot:build-image -pl spring-data-jdbc
+mvn -Pnative spring-boot:build-image -pl spring-data-jdbc
+
+echo "Building images for spring-data-jpa"
+mvn spring-boot:build-image -pl spring-data-jpa
+mvn -Pnative spring-boot:build-image -pl spring-data-jpa
+
+echo "Docker images inventory"
+docker images
